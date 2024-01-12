47c04e3689ec9742f5041bb8de051d8a834bd088
Vladimir <vladopecaros@gmail.com>
2024-01-12 14:41:38 +0100

    Try2

--- index.js
+++ index.js
@@ -28,33 +28,7 @@ mongoose.connect(process.env.DB_URI, dbOptions)
 .catch(err => console.error(err))
 
 
-router.get("/photos", async (req, res) => {
-    try{
-      const data = await schemas.Photos.find()
-        res.status(200).json(data).end();
-    }catch(err){
-      res.status(500).json(err).end();
-    }
-  });
-  router.post("/photos", async (req, res) => {
-      const {photoUrl, photoTitle, photoCategory} = req.body;
-  
-      const newSlika = new schemas.Photos({
-          photoUrl: photoUrl,
-          photoTitle: photoTitle,
-          photoCategory: photoCategory,
-          
-      });
-      const saveSlika = newSlika.save();
-      if(saveSlika){
-          res.status(200).send("Uspesno sacuvano");
-      }else{
-          res.status(500).send("Nije sacuvano");
-      }
-     
-  
-  
-  });
+
 
 
 const port = process.env.PORT || 4000;
